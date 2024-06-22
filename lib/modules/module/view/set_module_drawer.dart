f016b39576e3e9a5940c4e89b14d64351a7151b5
LM-Daniel <lozano.morales.daniel@outlook.com>
2024-06-22 14:30:43 -0700

    wip: agregando dorwer para editar modulos

---
+++ lib/modules/module/view/set_module_drawer.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SetModuleDrawer extends StatelessWidget {
+  const SetModuleDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
